2162cec87196e71fdf4ef6e8fab09016c3dd5d3e
shuosiw <shuosiw@gmail.com>
2022-02-26 20:53:28 +0800

    feat: 支持腾讯云 ocr 识别

--- captcha_app/ocrvendor/tencent.py
+++ captcha_app/ocrvendor/tencent.py
@@ -2,48 +2,47 @@
 # @Author: Source
 # @Date:   2022-02-20
 # @Last Modified by:   Source
-# @Last Modified time: 2022-02-20
+# @Last Modified time: 2022-02-26
 
-import os
+import re
 import sys
-import base64
+import json
 from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.ocr.v20181119 import ocr_client, models
 
+DEFAULT_REGION = 'ap-guangzhou'
+
 class tencent(object):
     """
     tencent cloud ocr class
     """
-
-    def __init__(self, apikey, secret, image, region='ap-guangzhou'):
+    def __init__(self, apikey, secret, region):
         cred = credential.Credential(apikey, secret)
         httpProfile = HttpProfile(reqMethod="POST", reqTimeout=60)
         httpProfile.endpoint = "ocr.tencentcloudapi.com"
-
         clientProfile = ClientProfile()
         clientProfile.signMethod = "TC3-HMAC-SHA256"
         clientProfile.httpProfile = httpProfile
-        self.client = ocr_client.OcrClient(cred, region, clientProfile)
-        self.image = image
+        if not region or region == 'null':
+            rg = DEFAULT_REGION
+            print('region not set, use default region: %s' % DEFAULT_REGION)
+        else:
+            rg = region
+        self.client = ocr_client.OcrClient(cred, rg, clientProfile)
 
 
-    def get_img_base64(self):
-        if not os.path.exists(self.image):
-            print 'image file not exists'
+    def recog_img(self, image):
+        req = models.GeneralAccurateOCRRequest()
+        req.from_json_string(json.dumps({"ImageBase64": image.decode()}))
+        try:
+            resp = self.client.GeneralAccurateOCR(req)
+        except TencentCloudSDKException as err:
+            print(err)
+            print('ERROR: cannot get recognition from tencentcloud')
             sys.exit(1)
-        with open(image, 'rb') as f:
-            img_base64 = str(base64.b64encode(f.read()), 'utf-8')
-        return img_base64
-
-    def recog_img(self, img_base64):
-        params = {"ImageBase64": image}
-        req = models.GeneralBasicOCRRequest()
-        req.from_json_string(json.dumps(params))
-        resp = self.client.GeneralBasicOCR(req)
-        for text in resp.TextDetections:
-            result = "".join(text.DetectedText)
-            result = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", result)
-        return '"recognition":"' + result + '"'
+        result = "".join([ a.DetectedText for a in resp.TextDetections ])
+        result = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", result)
+        return '"recognition":"%s"' % result
